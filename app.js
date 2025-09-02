c4467438dd941bce5615bf7d39e3bc71b5293b71
kaio2190 <santos.jesus.kaio@escola.pr.gov.br>
2025-09-02 10:55:22 -0300

    ok

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 alert('este é um jogo sigma brain rot');
 let mangomustard = 41;
 let chute = prompt('Escolha um número sigma de 41 a 67');
-
+console.log(mangomustard)
 if (chute == mangomustard) {
         alert("uoooooooou isso foi irado, você acertou");
 }       else{
