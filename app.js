e5d3a3f3bbb911716ab84fe29a73fa6e737b55ac
kaio2190 <santos.jesus.kaio@escola.pr.gov.br>
2025-09-02 10:51:10 -0300

    ok

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ let chute = prompt('Escolha um número sigma de 41 a 67');
 
 if (chute == mangomustard) {
         console.log("uoooooooou isso foi irado, você acertou");
+}       else{
+        alert("VOCE É BETA");
 }
