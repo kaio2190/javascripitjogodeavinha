169bb45e4125577cc51f9d7cd29831b68f09afb4
kaio2190 <santos.jesus.kaio@escola.pr.gov.br>
2025-09-08 12:27:32 -0300

    ok

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 alert('este é um jogo de adivnho. Após chutar o número, renicie a pagina para recomeçar.');
 let mangomustard = 41;
-let chute = prompt('Escolha um número sigma de 41 a 67');
+let chute = prompt('Escolha um número sigma de 0 a 41');
 console.log("mangomustard")
 
 if (chute == mangomustard) {
