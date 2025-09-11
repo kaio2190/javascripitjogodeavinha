1386ffbe2ed8c6a29e7bdb2910a397baf80db43f
kaio2190 <santos.jesus.kaio@escola.pr.gov.br>
2025-09-11 08:46:49 -0300

    ok

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ let tentativas = 1;
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+    alert(`Isso ai! Você descobriu o número secreto ${mangomustard}`);
 } else {
     alert('Você errou :(')
 }
 // enquanto chute não for igual ao n.s.
-while (chute != numeroSecreto) {
+while (chute != mangomustard) {
     chute = prompt('Escolha um número entre 1 e 10');
     // se chute for igual ao número secreto
-    if (chute == numeroSecreto) {
+    if (chute == mangomustard) {
        break;
     } else {
-        if (chute > numeroSecreto) {
+        if (chute > mangomustard) {
             alert(`O número secreto é menor que ${chute}`)
         } else {
             alert(`O número secreto é maior que ${chute}`)
@@ -30,4 +30,4 @@ while (chute != numeroSecreto) {
 }
 
 let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
-alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+alert(`Isso ai! Você descobriu o número secreto ${mangomustard} com ${tentativas} ${palavraTentativa}`);
