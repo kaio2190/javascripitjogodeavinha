5d6764b1e96d97c63c850cad7ab19e39d01d07f8
kaio2190 <santos.jesus.kaio@escola.pr.gov.br>
2025-09-11 08:43:27 -0300

    ok

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-alert('este é um jogo de adivnho. Após chutar o número, renicie a pagina para recomeçar.');
-let mangomustard = 41;
-let chute = prompt('Escolha um número sigma de 0 a 41');
-console.log("mangomustard")
+alert('Boas vindas ao jogo do número secretoooooouu');
+let mangomustard = 9;
+let mangomustard = parseInt(Math.random() * 10 + 1);
+console.log(mangomustard)
+let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
+let tentativas = 1;
 
-if (chute == mangomustard) {
-        alert(`oooooooou isso foi irado, você acertou`);
-}       else{
-        if(mangomustard > chute) {
-                alert(`baka o numero é menor que ${chute}`);
-        } else {
-                alert(`baka o numero é maior que ${chute}`);
-        }
-
-}
-if (mangomustard < chute) {
-        alert(`mané o numero é maior que ${chute}`);
+// se chute for igual ao número secreto
+if (chute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
-        alert(`mané o numero é maior que ${chute}`);
+    alert('Você errou :(')
 }
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+       break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`)
+        } else {
+            alert(`O número secreto é maior que ${chute}`)
+        }
+        // tentativas = tentativas + 1
+        tentativas++
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
---
+++ img/.js
@@ -0,0 +1,20 @@
+alert('este é um jogo de adivnho. Após chutar o número, renicie a pagina para recomeçar.');
+let mangomustard = 41;
+let chute = prompt('Escolha um número sigma de 0 a 41');
+console.log("mangomustard")
+
+if (chute == mangomustard) {
+        alert(`oooooooou isso foi irado, você acertou`);
+}       else{
+        if(mangomustard > chute) {
+                alert(`baka o numero é menor que ${chute}`);
+        } else {
+                alert(`baka o numero é maior que ${chute}`);
+        }
+
+}
+if (mangomustard < chute) {
+        alert(`mané o numero é maior que ${chute}`);
+} else {
+        alert(`mané o numero é maior que ${chute}`);
+}
