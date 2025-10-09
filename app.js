e9ed01d7de85a7c07268378853435225ea302460
kaio2190 <santos.jesus.kaio@escola.pr.gov.br>
2025-10-09 11:34:52 -0300

    yu

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let sexxo = document.querySelector('h1');
-sexxo.innerHTML = 'Jogo da gozada fatal';
+sexxo.innerHTML = 'Jogo de adivinho';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'digite um número de 1 e 10';
