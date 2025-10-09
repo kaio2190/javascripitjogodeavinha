b0fd7eb5d65c7dd117b52d7911954952eec35feb
kaio2190 <santos.jesus.kaio@escola.pr.gov.br>
2025-10-09 11:29:07 -0300

    asdv

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ sexxo.innerHTML = 'Jogo da gozada fatal';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'digite um número de 1 e 10';
+
+function verificarTentativa() {
+    console.log ('o botão foi clicado')
+}
