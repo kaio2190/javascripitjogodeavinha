05755ad3ae14aac3a1cda3cf758fd55f119f5c1b
kaio2190 <santos.jesus.kaio@escola.pr.gov.br>
2025-10-09 11:36:53 -0300

    as

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ sexxo.innerHTML = 'Jogo de adivinho';
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'digite um número de 1 e 10';
 
-function verificarTentativa() {
+function verificarChute() {
     console.log ('o botão foi clicado')
 }
