cf5d56ab018b6c75dc6e078da9937de9ac64eeab
kaio2190 <santos.jesus.kaio@escola.pr.gov.br>
2026-05-06 22:39:21 -0300

    sdsg

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -61,6 +61,6 @@ function reiniciarJogo() {
     numeroSecreto = gerarNumeroAleatorio();
     limparCampo();
     tentativas = 1;
-    exibirMensagemInicial();
+    exibirMensagemInicial();kaio2190/Projetinho.
     document.getElementById('reiniciar').setAttribute('disabled', true)
 }
